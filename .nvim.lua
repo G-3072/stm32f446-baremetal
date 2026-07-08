207d4a22f64a6f6c1ff5526c1d62c1ef9527f533
G-3072 <gaudenz.schweizer@icloud.com>
2026-07-08 18:26:43 +0200

    working on debug config

--- .nvim.lua
+++ .nvim.lua
@@ -1,1 +1,38 @@
+local dap = require("dap")
 
+local dap_cortex_debug = require("dap-cortex-debug")
+dap_cortex_debug.setup({})
+
+dap.configurations.c = {
+    {
+        name = 'Example debugging with OpenOCD',
+        type = 'cortex-debug',
+        request = 'launch',
+        servertype = 'openocd',
+        serverpath = 'openocd',
+        gdbPath = 'arm-none-eabi-gdb',
+        toolchainPath = '/Users/gasc/src-install/arm-none-eabi-10.3/bin',
+        toolchainPrefix = 'arm-none-eabi',
+        runToEntryPoint = 'main',
+        swoConfig = { enabled = false },
+        showDevDebugOutput = false,
+        gdbTarget = 'localhost:3333',
+        cwd = '${workspaceFolder}',
+        executable = '${workspaceFolder}/build/out',
+        configFiles = {
+            "interface/stlink.cfg",
+            "target/stm32f4x.cfg"
+        },
+        rttConfig = {
+            address = 'auto',
+            decoders = {
+                {
+                    label = 'RTT:0',
+                    port = 0,
+                    type = 'console'
+                }
+            },
+            enabled = true
+        }
+    }
+}
---
+++ core/main/src/main.c
@@ -0,0 +1,12 @@
+
+void _main(void){
+
+    int a = 5;
+    int b = 3;
+
+    int c = a + b;
+
+    for(c = a + b; c > (a-b); b++){
+        a = a + 0;
+    }
+}
